029127bea98119f8397433cd262936921c9f5579
薛宇睿 <89285446+Whaleman0423@users.noreply.github.com>
2022-04-26 20:58:27 +0800

    Add files via upload

--- return_link_all_line_bot_users_to_rich_menu.py
+++ return_link_all_line_bot_users_to_rich_menu.py
@@ -8,20 +8,20 @@ import json
 
 line_bot_api = LineBotApi(channel_access_token="line access token")
 
-line_user_list = []
+line_user_ids_list = []
 
 with open('CloudMasterLineBotUser.json', 'r', encoding='utf-8') as file:
     for line in file:
         line = json.loads(line)
-        line_user_list.append(line)
+        line_user_ids_list.append(line['line_user_id'])
     
     # 用戶總數
-    num_of_line_users = len(line_user_list)
+    num_of_line_users = len(line_user_ids_list)
     set_of_list = num_of_line_users // 500 + 1
     start = 0
     end = 500
     for i in range(set_of_list):
         if end > num_of_line_users:
             end = num_of_line_users
-            sub_list = line_user_list[start: end]
+            sub_list = line_user_ids_list[start: end]
             line_bot_api.link_rich_menu_to_users(user_ids=sub_list, rich_menu_id= )
